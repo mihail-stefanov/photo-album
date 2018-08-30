2db82c210671969ec00c596a08eb0acb10681b2f
Mihail Stefanov <mihail-stefanov@users.noreply.github.com>
2018-08-30 04:44:49 +0300

    Updated admin home page.

--- photoalbum/src/main/java/com/mihailstefanov/photoalbum/controllers/AdminController.java
+++ photoalbum/src/main/java/com/mihailstefanov/photoalbum/controllers/AdminController.java
@@ -1,25 +1,35 @@
 package com.mihailstefanov.photoalbum.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mihailstefanov.photoalbum.entities.User;
+import com.mihailstefanov.photoalbum.services.UserService;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	private final UserService userService;
+	
+	@Autowired
+	public AdminController(UserService userService) {
+		this.userService = userService;
+	}
 
 	@GetMapping("/home")
-	public String home(Model model, Authentication authentication) {
+	public String home(Model model) {
+		List<User> users = this.userService.getAllUsers();
 		model.addAttribute("view", "user/admin-home");
-		model.addAttribute("name", authentication.getName());
-		model.addAttribute("details", authentication.getDetails());
-		model.addAttribute("credentials", authentication.getCredentials());
-		model.addAttribute("authorities", authentication.getAuthorities());
+		model.addAttribute("users", users);
 		return "base-layout";
 	}
-	
 	
 	// TODO: Add all users list with the ability to delete users, convert to administrators, etc.
 	
